297bc6960afad3691d8bdfbef1afed43484ac91d
Mike <mihailemanuelghinea@gmail.com>
2020-01-01 21:02:22 +0000

    Add starting point for clox

---
+++ clox/cmake-build-debug/CMakeFiles/clox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clox.dir/main.c.obj"
+  "clox.exe"
+  "clox.exe.manifest"
+  "clox.pdb"
+  "libclox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
